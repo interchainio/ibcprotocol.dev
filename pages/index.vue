abb82ba49cbc8f2cd5abdec66bf2969438195968
Alan Woo <studio@alanwoo.ca>
2020-12-01 22:27:01 +0100

    Init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <nuxt-content :document="page" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const page = await $content('index').fetch()
+    return { page }
+  },
+}
+</script>

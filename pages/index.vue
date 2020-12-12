592b6816516b2c74e4257f7e85565a4d585bd101
Alan Woo <studio@alanwoo.ca>
2020-12-12 10:35:01 +0100

    Adjust sticky menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,41 @@
 <template>
   <div>
-    <div>
-      <div class="border-gray border-t px-8 md:px-12">
-        <Logo class="mx-auto w-full my-12" />
-      </div>
+    <div class="border-gray border-t px-8 md:px-12">
+      <Logo class="mx-auto w-full my-12" />
+    </div>
 
-      <div class="border-gray border-t border-b overflow-hidden">
-        <div class="uppercase flex items-center justify-between">
-          <ul class="flex -mx-2 ml-8">
-            <li class="mx-2">Documentation</li>
-            <li class="mx-2">FAQ</li>
-            <li class="mx-2">Forum</li>
-            <li class="mx-2">Discord</li>
-          </ul>
-          <div class="flex items-center">
-            <svg
-              class="mx-8"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="8" cy="8" r="7.5" stroke="#FFF1F1" />
-              <path
-                d="M15.5 8C15.5 11.9741 12.409 15.2263 8.5 15.4836V0.516402C12.409 0.773695 15.5 4.02588 15.5 8Z"
-                fill="#FFF1F1"
-                stroke="#FFF1F1"
-              />
-            </svg>
-            <div class="border-l border-gray px-8 py-2">EN</div>
-            <div class="border-l border-gray px-8 py-2">Search</div>
-          </div>
+    <div
+      class="border-gray border-t border-b overflow-hidden sticky top-0 bg-dark z-40"
+    >
+      <div class="uppercase flex items-center justify-between">
+        <ul class="flex -mx-2 ml-8">
+          <li class="mx-2">Documentation</li>
+          <li class="mx-2">FAQ</li>
+          <li class="mx-2">Forum</li>
+          <li class="mx-2">Discord</li>
+        </ul>
+        <div class="flex items-center">
+          <svg
+            class="mx-8"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="8" cy="8" r="7.5" stroke="#FFF1F1" />
+            <path
+              d="M15.5 8C15.5 11.9741 12.409 15.2263 8.5 15.4836V0.516402C12.409 0.773695 15.5 4.02588 15.5 8Z"
+              fill="#FFF1F1"
+              stroke="#FFF1F1"
+            />
+          </svg>
+          <div class="border-l border-gray px-8 py-2">EN</div>
+          <div class="border-l border-gray px-8 py-2">Search</div>
         </div>
       </div>
-
+    </div>
+    <div>
       <div class="mx-auto px-8 md:px-12 max-w-6xl relative">
         <h1
           class="text-lg md:text-3xl lg:text-5xl leading-tight uppercase mt-32 mb-16 md:mb-32"
